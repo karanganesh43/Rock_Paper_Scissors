e47250d0c97097dd9234d4ab755fb2377eccebc6
Karan Ganesh <karan.ganesh@hotmail.com>
2023-07-05 22:01:10 +1000

    fix indentation

--- javascript.js
+++ javascript.js
@@ -3,63 +3,62 @@ function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
-  }
-  
-  // Function to play a single round of Rock Paper Scissors
-  function playRound(playerSelection, computerSelection) {
+}
+
+// Function to play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
     const formattedPlayerSelection = playerSelection.toLowerCase();
-  
+
     if (
-      (formattedPlayerSelection === 'rock' && computerSelection === 'Scissors') ||
-      (formattedPlayerSelection === 'paper' && computerSelection === 'Rock') ||
-      (formattedPlayerSelection === 'scissors' && computerSelection === 'Paper')
+        (formattedPlayerSelection === 'rock' && computerSelection === 'Scissors') ||
+        (formattedPlayerSelection === 'paper' && computerSelection === 'Rock') ||
+        (formattedPlayerSelection === 'scissors' && computerSelection === 'Paper')
     ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (
-      (formattedPlayerSelection === 'rock' && computerSelection === 'Paper') ||
-      (formattedPlayerSelection === 'paper' && computerSelection === 'Scissors') ||
-      (formattedPlayerSelection === 'scissors' && computerSelection === 'Rock')
+        (formattedPlayerSelection === 'rock' && computerSelection === 'Paper') ||
+        (formattedPlayerSelection === 'paper' && computerSelection === 'Scissors') ||
+        (formattedPlayerSelection === 'scissors' && computerSelection === 'Rock')
     ) {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      return "It's a Tie!";
+        return "It's a Tie!";
     }
-  }
-  
-  // Function to play a 5-round game
-  function game() {
+}
+
+// Function to play a 5-round game
+function game() {
     let playerScore = 0;
     let computerScore = 0;
-  
+
     for (let round = 1; round <= 5; round++) {
-      const playerSelection = prompt(`Round ${round}: Choose rock, paper, or scissors:`);
-      const computerSelection = getComputerChoice();
-  
-      console.log(`You chose: ${playerSelection}`);
-      console.log(`Computer chose: ${computerSelection}`);
-  
-      const result = playRound(playerSelection, computerSelection);
-      console.log(result);
-  
-      if (result.includes('Win')) {
-        playerScore++;
-      } else if (result.includes('Lose')) {
-        computerScore++;
-      }
+        const playerSelection = prompt(`Round ${round}: Choose rock, paper, or scissors:`);
+        const computerSelection = getComputerChoice();
+
+        console.log(`You chose: ${playerSelection}`);
+        console.log(`Computer chose: ${computerSelection}`);
+
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes('Win')) {
+            playerScore++;
+        } else if (result.includes('Lose')) {
+            computerScore++;
+        }
     }
-  
+
     console.log('----- GAME OVER -----');
     console.log(`Final Score - You: ${playerScore}, Computer: ${computerScore}`);
-  
+
     if (playerScore > computerScore) {
-      console.log('Congratulations! You win the game!');
+        console.log('Congratulations! You win the game!');
     } else if (playerScore < computerScore) {
-      console.log('Computer wins the game! Better luck next time!');
+        console.log('Computer wins the game! Better luck next time!');
     } else {
-      console.log("It's a tie! The game ended in a draw.");
+        console.log("It's a tie! The game ended in a draw.");
     }
-  }
-  
-  // Start the game
-  game();
-  
+}
+
+// Start the game
+game();
