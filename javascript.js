f7b9bbe09c0fe26f6faac63960f0b51304c85890
Karan Ganesh <karan.ganesh@hotmail.com>
2023-07-05 20:13:55 +1000

    fix game. add 5 round system

--- javascript.js
+++ javascript.js
@@ -1,52 +1,65 @@
-const choices = ["Rock", "Paper", "Scissors"];
-
-// How the computer chooses
-function computerChoice() {
-    return choices[(Math.floor(Math.random() * choices.length))];
-}
-
-let playerChoice = prompt("Please choose a selection");
-
-// Call the function to get a choice from the computer
-computerChoice(choices);
-
-// Ignore these
-console.log(computerChoice(choices));
-console.log(playerChoice);
-
-
-function playRound(playerChoice, computerChoice) {
-    if ((playerChoice === "Rock") && (computerChoice === "Paper")) {
-        return "You Lose";
+// Function to generate computer's choice
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+  }
+  
+  // Function to play a single round of Rock Paper Scissors
+  function playRound(playerSelection, computerSelection) {
+    const formattedPlayerSelection = playerSelection.toLowerCase();
+  
+    if (
+      (formattedPlayerSelection === 'rock' && computerSelection === 'Scissors') ||
+      (formattedPlayerSelection === 'paper' && computerSelection === 'Rock') ||
+      (formattedPlayerSelection === 'scissors' && computerSelection === 'Paper')
+    ) {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (
+      (formattedPlayerSelection === 'rock' && computerSelection === 'Paper') ||
+      (formattedPlayerSelection === 'paper' && computerSelection === 'Scissors') ||
+      (formattedPlayerSelection === 'scissors' && computerSelection === 'Rock')
+    ) {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return "It's a Tie!";
     }
-    else if((playerChoice === "Rock") && (computerChoice === "Scissors")) {
-        return "You Win";
+  }
+  
+  // Function to play a 5-round game
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt(`Round ${round}: Choose rock, paper, or scissors:`);
+      const computerSelection = getComputerChoice();
+  
+      console.log(`You chose: ${playerSelection}`);
+      console.log(`Computer chose: ${computerSelection}`);
+  
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes('Win')) {
+        playerScore++;
+      } else if (result.includes('Lose')) {
+        computerScore++;
+      }
     }
-    else if ((playerChoice === "Rock") && (computerChoice === "Rock")) {
-        return "Tie. No Winner";
+  
+    console.log('----- GAME OVER -----');
+    console.log(`Final Score - You: ${playerScore}, Computer: ${computerScore}`);
+  
+    if (playerScore > computerScore) {
+      console.log('Congratulations! You win the game!');
+    } else if (playerScore < computerScore) {
+      console.log('Computer wins the game! Better luck next time!');
+    } else {
+      console.log("It's a tie! The game ended in a draw.");
     }
-    else if ((playerChoice === "Scissors") && (computerChoice === "Rock")) {
-        return "You Lose";
-    }
-    else if ((playerChoice === "Scissors") && (computerChoice === "Scissors")) {
-        return "Tie. No Winner";
-    }
-    else if ((playerChoice === "Scissors") && (computerChoice === "Paper")) {
-        return "You Win";
-    }
-    else if ((playerChoice === "Paper") && (computerChoice === "Paper")) {
-        return "Tie. No Winner";
-    }
-    else if ((playerChoice === "Paper") && (computerChoice === "Rock")) {
-        return "You Win";
-    }
-    else if ((playerChoice === "Paper") && (computerChoice === "Scissors")) {
-        return "You Lose";
-    }
-}
-
-// Call the function to play a round
-playRound(playerChoice, computerChoice);
-console.log(playRound(playerChoice, computerChoice));
-
-// function game(playRound), play 5 times, keep track of score, return winner
+  }
+  
+  // Start the game
+  game();
+  
